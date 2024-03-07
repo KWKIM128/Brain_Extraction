af2d182b0a3d7e9669e4ddfb17ac7aacd87d50f6
Keun Woo Kim <115262940+KWKIM128@users.noreply.github.com>
2024-03-07 16:26:56 +0000

    Update and rename evaluate.py to predicted_mask.py

--- code/predicted_mask.py
+++ code/predicted_mask.py
@@ -140,41 +140,4 @@ if __name__ == '__main__':
             )
         
         testinge_loop(model, test_loader, out_path)
-            
-        
-        predicted = sorted(glob(out_path + '*.nii.gz'))
-        predicted.sort(key=lambda x: int(x.split('\\')[-1].split('.')[0]))
-        ground_truth = sorted(glob(gts[i]))
-        
-        dices = []
-        precisions = []
-        recalls = []
-        
-        for j in range(len(predicted)):
-            pm = nib.load(predicted[j]).get_fdata()
-            gt = nib.load(ground_truth[j]).get_fdata()
-            
-            pm = pm > 0.5
-            gt = gt > 0.5
-            
-            
-            dice = dice_metrics(pm, gt)
-            precision = precision_metrics(pm, gt)
-            recall = recall_metrics(pm, gt)
-            
-            dices.append(dice)
-            precisions.append(precision)
-            recalls.append(recall)
-        
-        print(outputs[i])
-        print(s.median(dices))
-        print(s.stdev(dices))
-        
-        
-        np.savetxt(out_path+'dice.txt', dices)
-        np.savetxt(out_path+'precision.txt', precisions)
-        np.savetxt(out_path+'recall.txt', recalls)
-        
-        
-    
-            
+
