79b37165ecd106c96ec1af4296de88f1076c5373
Keun Woo Kim <115262940+KWKIM128@users.noreply.github.com>
2024-03-09 13:43:58 +0000

    Update post_processing.py

--- code/post_processing.py
+++ code/post_processing.py
@@ -15,7 +15,7 @@ def post_processing(mask):
     Original code: https://github.com/MICLab-Unicamp/CONSNet/blob/master/infer.py
     """
     # Threshold to get rid of noise
-    mask = np.where(mask >= 0.85, 1, 0)
+    mask = np.where(mask >= 0.5, 1, 0)
 
     labels, n = ndi.measurements.label(mask)
     hist = np.histogram(labels.flat, bins=(n + 1), range=(-0.5, n + 0.5))[0]
